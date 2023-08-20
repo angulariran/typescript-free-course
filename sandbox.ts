7ca805feea4fd3423c1f6dbccdfc331a72f101dc
angulariran <angulariran01@gmail.com>
2023-08-20 22:50:39 +0330

    basic types

--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,10 @@
-const character = 'mario';
+let checkout:number=-65;
+let firstName:string='sajjadmaneshi';
+let isRegister:boolean=true;
+let age=28;
 
-console.log(character);
+const sum = (a:number,b:number)=>{
+  return (a+b)*2;
+}
 
-const inputs = document.querySelectorAll('input');
-
-inputs.forEach(input => {
-  console.log(input);
-});
+console.log(sum(3,5));
