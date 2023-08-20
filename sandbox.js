77324ad417a1325231fadebb58e5531f36cc19d5
angulariran <angulariran01@gmail.com>
2023-08-20 22:43:07 +0330

    basic type

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var checkout = -65;
+var firstName = 'sajjadmaneshi';
+var isRegister = true;
+var age = 28;
+var sum = function (a, b) {
+    return (a + b) * 2;
+};
+console.log(sum(3, 5));
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+let checkout:number=-65;
+let firstName:string='sajjadmaneshi';
+let isRegister:boolean=true;
+let age=28;
+
+const sum = (a:number,b:number)=>{
+    return (a+b)*2;
+}
+
+console.log(sum(3,5));
