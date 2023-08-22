291a40e5fb077d94c60f815c2fa0b70de8ea9a95
angulariran <angulariran01@gmail.com>
2023-08-22 21:11:02 +0330

    lesson-3 array and object

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,17 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+//array
+var numbers = [1, 3, 5, 7, 8];
+numbers.push(9);
+numbers.pop();
+var specificNum = numbers[2];
+var names = ['sajjad', 'ali', 'hassan'];
+var mixed = [1, 'salam', true];
+mixed.push(2);
+mixed.push('salam');
+//object
+var user;
+user = {
+    name: 'sajjad',
+    family: 'maneshi',
+    height: 150,
+    age: 10,
+};
